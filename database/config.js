fd850e2170a7585fbfd44c66779fc6901d44974b
WarrenMfg <warrenmfg@icloud.com>
2020-01-24 18:23:47 -0500

    beginning config; switching to master to make my first pull

--- database/config.js
+++ database/config.js
@@ -1,2 +1,16 @@
 const faker = require('faker');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  user: 'root',
+  password: '',
+  database: 'about'
+});
+connection.connect();
 
+
+for (let i = 0; i < 1; i++) {
+
+
+
+
+}
