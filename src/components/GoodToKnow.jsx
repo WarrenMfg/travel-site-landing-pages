d4483376fcbad6006e8bf308dec2d2e245a91359
WarrenMfg <warrenmfg@icloud.com>
2020-01-27 17:15:00 -0500

    completed good to know popup

--- src/components/GoodToKnow.jsx
+++ src/components/GoodToKnow.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import styles from '../css/GoodToKnowCSS.js';
 import HotelClass from './HotelClass.jsx';
+import {jsx} from '@emotion/core';
 
 class GoodToKnow extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      infoPopup: false
+    };
+    this.handleInfoMouseOver = this.handleInfoMouseOver.bind(this);
+    this.handleInfoMouseLeave = this.handleInfoMouseLeave.bind(this);
+  }
+
+  handleInfoMouseOver() {
+    this.setState({infoPopup: true});
+  }
+
+  handleInfoMouseLeave() {
+    this.setState({infoPopup: false});
   }
 
   render() {
     return (
       <styles.goodToKnowContainer>
         <h4>Good to know</h4>
-        <h5>HOTEL CLASS</h5>
+        <h5>HOTEL CLASS <i css={styles.i} className="fas fa-info-circle" onMouseOver={this.handleInfoMouseOver} onMouseLeave={this.handleInfoMouseLeave}></i></h5>
+        {this.state.infoPopup ?
+          <styles.infoPopup>
+            <div>
+              <p>Star ratings indicate the general level of features and amenities to expect. They are provided to TripAdvisor by third-party partners such as Giata.</p>
+            </div>
+          </styles.infoPopup> :
+          null
+        }
+
         <HotelClass
           hotelClass={this.props.hotelClass}
         />
--- src/css/GoodToKnowCSS.js
+++ src/css/GoodToKnowCSS.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 const styles = {
   goodToKnowContainer: styled.div({
+    position: 'relative',
     borderBottom: '1px solid rgb(229, 229, 229)',
     paddingBottom: '14px',
     '& h4': {
@@ -11,6 +12,27 @@ const styles = {
     '& h5': {
       fontSize: '12px',
       margin: '16px 0 8px'
+    },
+    '& i': {
+      '&:hover': {
+        color: 'rgb(0, 129, 113)'
+      }
+    }
+  }),
+
+  infoPopup: styled.div({
+    position: 'absolute',
+    width: '300px',
+    transform: 'translate(-19%, -130%)',
+    filter: 'drop-shadow(0 0 5px black)',
+    '& div': {
+      clipPath: 'polygon(0 0, 100% 0, 100% 75%, 60% 75%, 50% 100%, 40% 75%, 0 75%)',
+      backgroundColor: 'white',
+      padding: '15px 15px 12.5% 15px'
+    },
+    '& p': {
+      margin: '0',
+      fontSize: '12px'
     }
   }),
 
