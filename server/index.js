3a52b58707b4d5b7bcdf1fa94c555f4fb054b433
WarrenMfg <warrenmfg@icloud.com>
2020-02-13 10:39:10 -0500

    adjusted app.get for bundle.js.br

--- server/index.js
+++ server/index.js
@@ -7,8 +7,13 @@ const router = require('./router.js');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.use('/bundle.js.br', express.static(__dirname + '/../public/bundle.js.br')); // for proxy server
+// app.use('/bundle.js.br', express.static(__dirname + '/../public/bundle.js.br')); // for proxy server
 app.use('/api/about', router);
+app.get('/bundle.js.br', (req, res) => {
+  const bundle = fs.createReadStream('../public/bundle.js.br');
+  res.set({'Content-Encoding': 'br'});
+  bundle.pipe(res);
+});
 app.use('/:id', express.static(__dirname + '/../public'));
 
 app.listen(process.env.PORT || 50001, console.log(`Listening on port ${process.env.PORT || 50001}`));
