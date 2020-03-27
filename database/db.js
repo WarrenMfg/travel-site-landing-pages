dbc3b75aa9d353d4069989ba1faf36352286c11f
WarrenMfg <warrenmfg@icloud.com>
2020-03-27 13:35:35 -0400

    revisiting database

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: process.env.RDS_HOST || 'localhost',
   user: process.env.RDS_USERNAME || 'root',
-  password: process.env.RDS_PASSWORD || undefined,
+  password: process.env.RDS_PASSWORD || '', // undefined
   database: 'HRR43_FEC'
 });
 connection.connect();
